146a0c7544a9bc4a6cf5f1e4cd353c8fd7f76897
Jesse Hoobergs <jesse@hoobergs.be>
2016-09-17 09:12:10 -0400

    Working location

--- Android/app/src/main/java/be/tomberndjesse/picaloc/LoggedInActivity.java
+++ Android/app/src/main/java/be/tomberndjesse/picaloc/LoggedInActivity.java
@@ -1,17 +1,37 @@
 package be.tomberndjesse.picaloc;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Created by jesse on 17/09/2016.
  */
-public class LoggedInActivity extends AppCompatActivity{
+public class LoggedInActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private boolean mRequestingLocationUpdates = true;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+
+    SensorDataInterface sensorDataInterface;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +42,89 @@ public class LoggedInActivity extends AppCompatActivity{
             startActivity(new Intent(this, SignInActivity.class));
             finish();
         }
+    }
+
+    public void enableLocationUpdates(int interval, int fastestInterval) {
+        createLocationRequest(interval, fastestInterval);
+        buildGoogleApiClient();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    public Location getLocation() {
+        return mCurrentLocation;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void createLocationRequest(int interval, int fastestInterval) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(interval);
+        mLocationRequest.setFastestInterval(fastestInterval);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //TODO:
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    public void setSensorDataInterface(SensorDataInterface listener){
+        sensorDataInterface = listener;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        if(sensorDataInterface != null)
+            sensorDataInterface.locationChanged(location);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void stopLocationUpdates() {
+        if(mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationUpdates();
     }
 }
