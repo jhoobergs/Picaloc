acbf1aea69dfd5f24d7116d43d6c7fcd5ff9d09b
Tom Gijselinck <tomgijselinck@gmail.com>
2016-09-17 12:46:31 -0400

    Change routes

--- backend/picaloc/config/routes.rb
+++ backend/picaloc/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+
+  post 'posts/add', to: 'posts#create'
+  post 'posts/get', to: 'posts#index'
 end
