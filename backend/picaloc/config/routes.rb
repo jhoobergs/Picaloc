baaffcb2a7b0c183162287ab4daa744ee917611e
Tom Gijselinck <tomgijselinck@gmail.com>
2016-09-17 17:58:13 -0400

    Enable liking posts

--- backend/picaloc/config/routes.rb
+++ backend/picaloc/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :user_locations
+  resources :likes
 
   post 'posts/add', to: 'posts#create'
   post 'posts/get', to: 'posts#index'
