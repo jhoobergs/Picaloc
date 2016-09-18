b3c81d4e92672f9de17c7ea8a75eb051f9a68b60
Bernd Schrooten <schrootenbernd@gmail.com>
2016-09-18 01:28:27 -0400

    Done

--- Web/public/app/overview/map.directive.js
+++ Web/public/app/overview/map.directive.js
@@ -46,12 +46,40 @@
                     searchBox.setBounds($scope.map.getBounds());
                 });
 
+                // Listen for the event fired when the user selects a prediction and retrieve
+                // more details for that place.
                 searchBox.addListener('places_changed', function() {
                     var places = searchBox.getPlaces();
 
                     if (places.length == 0) {
                         return;
                     }
+
+                    // For each place, get the icon, name and location.
+                    var bounds = new google.maps.LatLngBounds();
+                    var done = false;
+                    places.forEach(function(place) {
+                        if (!place.geometry) {
+                            console.log("Returned place contains no geometry");
+                            return;
+                        }
+
+                        if (done) {
+                            return
+                        }
+
+                        // Create a marker for each place.
+                        placeMarker(place.geometry.location);
+                        done = true;
+
+                        if (place.geometry.viewport) {
+                            // Only geocodes have viewport.
+                            bounds.union(place.geometry.viewport);
+                        } else {
+                            bounds.extend(place.geometry.location);
+                        }
+                    });
+                    $scope.map.fitBounds(bounds);
                 });
 
                 $scope.marker = new maps.Marker({
