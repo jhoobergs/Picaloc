061ac74a05c514ebc4b17e5836bd932859f5bf2f
Tom Gijselinck <tomgijselinck@gmail.com>
2016-09-18 01:10:07 -0400

    Implement broadcasting posts

--- backend/picaloc/app/controllers/posts_controller.rb
+++ backend/picaloc/app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
+require 'net/http'
+require 'uri'
 class PostsController < ApplicationController
   before_action :set_post, only: [:destroy, :update]
 
   def index
+    #TODO: include whether this user has liked the picture or not
     render :json => Post.all
   end
 
@@ -11,6 +14,7 @@ class PostsController < ApplicationController
       new_params[:user_id] = @payload['user_id']
       @post = Post.new(new_params)
       if @post.save
+        broadcast_post
         render json: {status: :created}
       else
         render json: @post.errors, status: :unprocessable_entity
@@ -32,6 +36,51 @@ class PostsController < ApplicationController
   end
 
   private
+  def broadcast_post
+    # get all users
+    # calculate distances of all users to this user
+    # sort all the users on their distance to this user
+    # calculate the number N users this user can reach
+    # select the N closest users and send them this post
+    user_locations = {}
+    UserLocation.all.each do |user_location|
+      user_locations[user_location[:user_id]] =
+          calculate_distance(user_location[:longitude],
+                             user_location[:latitude],
+                             @post[:longitude],
+                             @post[:latitude])
+    end
+    user_locations.sort_by { |user_id, distance| distance }
+    max_user_count = [get_score, @post.likes.count].min
+    user_locations.keys[0..max_user_count].each do |key|
+      send_post key, @post[:id]
+    end
+  end
+
+  def get_score
+    (1 + @post.likes.count) * 10
+  end
+
+  def calculate_distance(lon1, lat1, lon2, lat2)
+    Math.sqrt((lon1 - lon2).abs**2 + (lat1 - lat2).abs**2)
+  end
+
+  # TODO: implement push notification
+  def send_post(user_id, post_id)
+    # add post to user
+    # send push notification using firebase api
+    UserReceivedPost.create({:user_id => user_id, :post_id => post_id})
+    # header = {'Content-Type': 'application/json'}
+    # uri = URI.parse('https://fcm.googleapis.com/fcm/send/')
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # request = Net::HTTP::Post.new(uri.path, header)
+    # request.body = {
+    #     :to => @token,
+    #     :data => @post
+    # }.to_json
+    # http.request(request)
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
